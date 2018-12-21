5494ea180f23357d2267fc4c45696265c0df2845
musonera <musosoaimee@gmail.com>
2018-12-21 16:13:43 +0200

    add link

--- js/script.js
+++ js/script.js
@@ -1,24 +1,34 @@
-var myQuiz = function(one, two ,three){
-    return one + two + three;
-    
-    }
-    
+  
     $(document).ready(function(){
       $(".begin").click(function(){
-        $("#test").fadeToggle("slow");
+        $("#quiz").fadeToggle("slow");
       });
     
       $("form#quiz").submit(function(event){
         var question1 = parseInt($("input:radio[name=question1]:checked").val());
         var question2 = parseInt($("input:radio[name=question2]:checked").val());
-        var question = parseInt($("input:radio[name=question3]:checked").val());
-        var question = parseInt($("input:radio[name=question4]:checked").val());
-        var question = parseInt($("input:radio[name=question5]:checked").val());
+        var question3 = parseInt($("input:radio[name=question3]:checked").val());
+        var question4 = parseInt($("input:radio[name=question4]:checked").val());
+        var question5 = parseInt($("input:radio[name=question5]:checked").val());
+        var score = 0;
         
-    
-        var result = quizResults(question1, question2 ,question3,question4,question5);
-    
-        $("#display").text("Your score is:" + result);
+ if( question1===+8){
+     score+=8;
+}
+if(question2===+8){
+  score+=8;
+}
+if(question3===+8){
+  score+=8;
+}
+if(question4===+8){
+  score+=8;
+}
+if(question5){
+  score+=8;
+}
+
+        $("#display").quiz("Your score is:" + result);
     
         event.preventDefault();
         $(".final").show();
